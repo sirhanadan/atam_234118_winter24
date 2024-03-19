ee67d0d23eb066e0bdeac43d1e9ddfcd278a8935
sirhanadan <107433544+sirhanadan@users.noreply.github.com>
2024-03-19 21:15:05 +0200

    unauthorised access to memory error
    somewhere it accesses something it shouldn't, maybe the pushs and pops of all the registers is incorrect?

--- hw3/part2/ili_handler.asm
+++ hw3/part2/ili_handler.asm
@@ -10,6 +10,7 @@ my_ili_handler:
   pushq %rbp
   mov %rsp, %rbp
   
+  pushq %rdi
   pushq %rsi
   pushq %rax
   pushq %rbx
@@ -31,7 +32,7 @@ my_ili_handler:
   
   
   
-  #if an exception of type error occurs, the processor saves the address of the instruction that caused the fault into rip
+  #if an exception of type opcode fault occurs, the processor saves the address of the instruction that caused the fault into rip
   #the value of rip is saved on the stack right before we entered the handler
   #so basically it exists 8 bytes above rbp
   
@@ -39,26 +40,88 @@ my_ili_handler:
   mov 8(%rbp), %rax
   
   #load the opcode
-  mov 0(%rax), %dl
+  mov 0(%rax), %dil
   
   #when dealing with an opcode of size 2 bytes at most:
   #if the first byte is 0x0F then it is one byte
-  cmpb $0x0F, %dl
+  cmpb $0x0F, %dil
+  jne one_byte_opcode
+  
+  #else: 2 bytes - the first one is irrelevant bc 0x0F
+  mov 1(%rax), %dil
+  inc %rcx #the return address would be 2 bytes after rax 
+  
+one_byte_opcode:
+  inc %rcx #the return address would be 1 byte after rax
+  add %rax, %rcx 
+  
+  #call the what_to_do function:
+  #save the (only) registers we need:
+  pushq %rdi
+  pushq %rcx
+  #####pushq %rax - no need because we saved the return address in rcx
+  
+  call what_to_do
+  
+  popq %rcx
+  popq %rdi
+  
+  cmp $0, %rax
+  jne cont_our_handler
+  
+  #continue with the original handler:
+  
+  #restore the registers:
+
+  popq %r15
+  popq %r14
+  popq %r13
+  popq %r12
+  popq %r11
+  popq %r10
+  popq %r9
+  popq %r8
+  popq %rdx
+  popq %rcx
+  popq %rbx
+  popq %rax
+  popq %rsi
+  popq %rdi
+  popq %rbp
+  #jump to old handler:
+  jmp *old_ili_handler
+  iretq
+
+
+cont_our_handler:
+	#the value of rdi is the ret_val of what_to_do
+	mov %rax, %rdi
+	mov %rcx, 8(%rbp) #rbp is the return address and in our case we saved it in rcx
+	
+
   
   
   
+  #restore the registers:
+
+  popq %r15
+  popq %r14
+  popq %r13
+  popq %r12
+  popq %r11
+  popq %r10
+  popq %r9
+  popq %r8
+  popq %rdx
+  popq %rcx
+  popq %rbx
+  popq %rax
+  popq %rsi
+  # popq %rdi
+  # popq %rbp
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  #addq $2, (%rsp) #??? ro ignore the last 2 pops
   
  
-  
   
   iretq
